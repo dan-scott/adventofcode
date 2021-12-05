b96eb985e681e724637f89afa76ca31311ccc5d8
Dan Scott <dan.scott@xero.com>
2021-12-06 11:26:39 +1300

    2021 go day 5 add tests & benchmarks

---
+++ go/aoc_2021/internal/day05/day05_test.go
@@ -0,0 +1,39 @@
+package day05
+
+import "testing"
+
+func TestDay05_Part1(t *testing.T) {
+	d := New()
+	d.Open()
+	answer := d.Part1()
+	if answer != "5442" {
+		t.Fatalf("Expected 5442 but got %s", answer)
+	}
+}
+
+func TestDay05_Part2(t *testing.T) {
+	d := New()
+	d.Open()
+	answer := d.Part2()
+	if answer != "19571" {
+		t.Fatalf("Expected 19571 but got %s", answer)
+	}
+}
+
+func BenchmarkDay05_Part1(b *testing.B) {
+	d := New()
+	d.Open()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		d.Part1()
+	}
+}
+
+func BenchmarkDay05_Part2(b *testing.B) {
+	d := New()
+	d.Open()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		d.Part2()
+	}
+}
