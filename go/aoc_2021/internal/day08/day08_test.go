88f4c48ae436184827413f0834602d9b2eb6b56e
Dan Scott <dan.scott@xero.com>
2021-12-09 10:11:59 +1300

    2021 go day 8 fixed tests & add input to source control

--- go/aoc_2021/internal/day08/day08_test.go
+++ go/aoc_2021/internal/day08/day08_test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestDay08_Part1(t *testing.T) {
 	d := &day08{}
 	d.Open()
-	const expected = ""
+	const expected = "532"
 	answer := d.Part1()
 	if answer != expected {
 		t.Fatalf("Expected %s but got %s", expected, answer)
@@ -13,24 +13,29 @@ func TestDay08_Part1(t *testing.T) {
 }
 
 func TestDay08_Part2(t *testing.T) {
-	lines := []string{
-		"acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf",
-		//"be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe",
-		//"edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc",
-		//"fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg",
-		//"fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb",
-		//"aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea",
-		//"fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb",
-		//"dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe",
-		//"bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef",
-		//"egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb",
-		//"gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce",
-	}
 	d := &day08{}
-	d.loadLines(lines)
-	const expected = ""
+	d.Open()
+	const expected = "1011284"
 	answer := d.Part2()
 	if answer != expected {
 		t.Fatalf("Expected %s but got %s", expected, answer)
+	}
+}
+
+func BenchmarkDay08_Part1(b *testing.B) {
+	d := New()
+	d.Open()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		d.Part1()
+	}
+}
+
+func BenchmarkDay08_Part2(b *testing.B) {
+	d := New()
+	d.Open()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		d.Part2()
 	}
 }
